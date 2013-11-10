21a68891d096be98414db55bd97e84ced54b2d8f
bladymir <jose@albatross.pe>
2013-11-09 21:12:03 -0500

    clase 3

---
+++ src/app/clase3/interfaces/posicion/Teodolito.java
@@ -0,0 +1,24 @@
+package app.clase3.interfaces.posicion;
+
+public class Teodolito {
+
+    public static void main(String[] args) {
+        
+        Position carro = new Auto(20, 30);
+        
+        Position jose = new Persona(50, 20);
+        
+
+        Teodolito.getDistancia(carro, jose);
+        
+    }
+
+    public static void getDistancia(Position p1, Position p2) {
+
+        double d1 = p1.getLatitud() - p2.getLatitud();
+
+        double d2 = p1.getLongitud() - p2.getLongitud();
+
+        System.out.println(Math.sqrt(d1 * d1 + d2 * d2));
+    }
+}
